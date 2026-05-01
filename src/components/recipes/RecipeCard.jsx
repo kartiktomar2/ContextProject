13f11aea9bd6aedf355d38d4916427212dc45fbd
kartiktomar2 <kartiktomar1717@gmail.com>
2026-05-01 11:59:24 +0530

    updated recipe card

--- src/components/recipes/RecipeCard.jsx
+++ src/components/recipes/RecipeCard.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import { useMealPlanContext } from '../../contexts/MealPlanContext'
 
 const RecipeCard = ({recipe}) => {
+  const {addMealToDay, setSelectedRecipe}= useMealPlanContext()
   return (
-    <div>
-          Recipe Name: {recipe.name}
-          Meal Type: {recipe.mealType}
-          Diet: {recipe.diet}
-          Prep Time: {recipe.prepTime} min 
+   <div>
+      <h3>Recipe Name: {recipe.name}</h3>
+      <p>Recipe Meal Type: {recipe.mealType}</p>
+      <p>Recipe Diet: {recipe.diet}</p>
+      <p>Recipe Prep Time: {recipe.prepTime} min</p>
+
+      <button onClick={() => addMealToDay("monday", recipe)}>
+        Add To Monday
+      </button>
+
+      <button onClick={() => setSelectedRecipe(recipe)}>
+        View Details
+      </button>
     </div>
   )
 }
